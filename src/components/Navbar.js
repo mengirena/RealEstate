8d87b3e4a01d580a48e68ca454d77676fc27f84d
Irena Chen <irena1009@gmail.com>
2021-08-28 14:29:53 -0700

    add Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { menuData } from '../data/MenuData'
 
 
 const Nav = styled.nav`
@@ -14,13 +15,20 @@ const Logo = styled(Link)`
 
 const MenuBars = styled.i``
 const NavMenu = styled.div``
+const NavMenuLinks = styled(Link)``
 
 const Navbar = () => {
     return (
         <Nav>
             <Logo>ELIXR</Logo>
             <MenuBars />
-            <NavMenu />
+            <NavMenu>
+                {menuData.map((item, index)=>{
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                })}
+            </NavMenu>
             <h1>Navbar</h1>
         </Nav>
     )
