e8d8ffa03c571376cc0e10194f752e0e1721d38a
Irena Chen <irena1009@gmail.com>
2021-08-27 23:35:52 -0700

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-    <h1>Whats</h1>
+    <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
