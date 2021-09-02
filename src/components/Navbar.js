08430e3e63ce609dbe5c143f8ab6945952b32df1
Irena Chen <irena1009@gmail.com>
2021-09-02 15:23:05 -0700

    create global styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,51 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components/macro'
 import { Link } from 'react-router-dom'
 import { menuData } from '../data/MenuData'
+import { Button } from './Button'
 
 
 const Nav = styled.nav`
     height: 60px;
-    background: red;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width:100%;
+    background: #000;
+`
+
+const NavLink = css`
+    color: #fff;
+    display: flex;
+    align-items:center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
 `
 
 const Logo = styled(Link)`
-    color: #fff;
+    ${NavLink}
+    font-style: italic;
 `
 
 const MenuBars = styled.i``
-const NavMenu = styled.div``
-const NavMenuLinks = styled(Link)``
+const NavMenu = styled.div`
+    display: flex;
+    align-item: center;
+    margin-right:-48px;
+`
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+`
+
+const NavBtn = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 24px;
+`
 
 const Navbar = () => {
     return (
@@ -23,13 +53,15 @@ const Navbar = () => {
             <Logo>ELIXR</Logo>
             <MenuBars />
             <NavMenu>
-                {menuData.map((item, index)=>{
+                {menuData.map((item, index)=>(
                     <NavMenuLinks to={item.link} key={index}>
                         {item.title}
                     </NavMenuLinks>
-                })}
+                ))}
             </NavMenu>
-            <h1>Navbar</h1>
+            <NavBtn>
+                <Button to="/contact">Contact Us</Button>
+            </NavBtn>
         </Nav>
     )
 }
