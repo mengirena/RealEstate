83387ba98b335c2c32db102be503b046d3da628a
Irena Chen <irena1009@gmail.com>
2021-09-03 11:35:48 -0700

    add data structurea &images

---
+++ src/data/SliderData.js
@@ -0,0 +1,61 @@
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: '$4,280,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'Luxury Villa in Bali'
+    },
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House in San Jose'
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$8,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House in Calabasas'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House in Houston'
+    },
+]
+
+export const SliderDataTwo = [
+    {
+        title: 'House in Las Vegas, Nevada',
+        price: '$8,980,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFive,
+        alt: 'House in Las Vegas'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$3,460,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageSix,
+        alt: 'House in Houston'
+    },
+    {
+        title: 'House in Alajuela, Costa Rica',
+        price: '$5,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageSeven,
+        alt: 'House'
+    },
+]
