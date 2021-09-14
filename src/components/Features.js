c344f8b5e0e37e126a12217c34a8f5d69dbb3c64
Irena Chen <irena1009@gmail.com>
2021-09-14 12:45:21 -0700

    add features

--- src/components/Features.js
+++ src/components/Features.js
@@ -16,12 +16,63 @@ const Container = styled.div`
     position: relative;
 `
 const Wrap = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
 `
 
-const ColumnLeft = styled.div``
-const Content = styled.div``
-const ColumnRight = styled.div``
-const Image = styled.img``
+const ColumnLeft = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px -15px;
+    justify-content: flex-start;
+    padding: 1rem;
+
+`
+
+const Content = styled.div`
+    flex: 0 0 50%;
+
+    @media screen and (max-width:768px) {
+        flex: 0 0 100%;
+        max-width: 100%;
+        margin-top: 250px;
+
+    }
+
+    h1 {
+        margin-bottom: 2rem;
+        font-size: 2rem;
+    }
+
+    p {
+        margin-bottom: 1rem;
+        line-height: 1.5;
+    }
+
+`
+const ColumnRight = styled.div`
+    position: absolute;
+    top: -80px;
+    right: 0;
+    max-width: 850px;
+    height: 140%;
+    width: 45%;
+    padding-left: 1rem;
+
+    @media screen and ( max-width: 760px) {
+        height: 320px;
+        top: -65px;
+        width: 70%;
+        margin: 0 auto;
+        left: 0;
+    }
+    
+`
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
 
 
 
@@ -33,7 +84,7 @@ const Features = () => {
                     <ColumnLeft>
                         <Content>
                             <h1>Stunning Interior</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta cum quas tempore dolores ratione reiciendis suscipit magni quae repellat veniam!</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta cum quas tempore dolores ratione reiciendis suscipit magni quae repellat veniam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor magnam numquam quasi aspernatur maiores dolorum.</p>
                             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, asperiores.</p>
                             <Button to="/homes">Learn More</Button>
                         </Content>
